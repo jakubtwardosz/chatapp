232c1a9dcad64cf3c4a847ff47f3f5638f2480cc
Jakub Twardosz <jakubtwardosz@protonmail.com>
2024-02-05 20:00:57 +0100

    Add joinChatRoom method

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import { Col, Row, Container } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WaitingRoom from './components/waitingroom';
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 function App() {
+  const [conn, setConnection] = useState();
+
+  const joinChatRoom = async (username, chatroom) => {
+    try {
+      // initialize a connection
+      const conn = new HubConnectionBuilder()
+        .withUrl("http://localhost:5157/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      // set up handler
+      conn.on("JoinSpecificChatRoom", (username, msg) => {
+        console.log("msg: ", msg);
+      });
+
+      await conn.start();
+      await conn.invoke("JoinSpecificChatRoom", {username, chatroom});
+
+      setConnection(conn);
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div>
       <Container>
@@ -12,7 +39,7 @@ function App() {
             <h1 className='font-weight-light'>Hello!</h1>
           </Col>
         </Row>
-        <WaitingRoom></WaitingRoom>
+        <WaitingRoom joinChatRoom={joinChatRoom}></WaitingRoom>
       </Container>
     </div>
   );
